6f0e1bc12e4445d853b2372321f63b6f22c493d2
xiaoxibukeai-LTC <1062147677@qq.com>
2025-11-12 23:03:20 +0800

    Initial commit or your message

---
+++ Assets/Player.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public PlayerInput PlayerInput;
+    public Animator anim;
+
+    [Header("Movement Variables")]
+    public float speed;
+    public float jumpForce;
+    public float jumpCutMultiplier = 0.5f;
+    public float normalGravity;
+    public float fallGravity;
+    public float jumpGravity;
+
+    public int facingDirection = 1;
+
+    public Vector2 moveInput;
+    private bool jumpPressed;
+    private bool jumpReleased;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb.gravityScale = normalGravity;
+    }
+
+    void Update()
+    {
+        Flip();
+        HandleAnimations();
+    }
+    
+    void FixedUpdate()
+    {
+        ApplyVariableGravity();
+        CheckGrounded();
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        float targetSpeed = moveInput.x * speed;
+        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleJump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpPressed = false;
+            jumpReleased = false;
+        }
+        if (jumpReleased)
+        {
+            if (rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            }
+            jumpReleased = false;
+        }
+    }
+
+    void ApplyVariableGravity()
+    {
+        if (rb.linearVelocity.y < -0.1f)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else if (rb.linearVelocity.y > 0.1f)
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
+
+
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void HandleAnimations()
+    {
+        anim.SetBool("isJumping", rb.linearVelocity.y > .1f);
+        anim.SetBool("isGrounded", isGrounded);
+        
+        anim.SetFloat("yVelocity",rb.linearVelocity.y);
+
+        anim.SetBool("isIdle", Mathf.Abs(moveInput.x) < .1f && isGrounded);
+        anim.SetBool("isWalking", Mathf.Abs(moveInput.x) > .1f && isGrounded);
+    }
+
+    void Flip()
+    {
+        if(moveInput.x > 0.1f)
+        {
+            facingDirection = 1;
+        }
+        else if(moveInput.x < -0.1f)
+        {
+            facingDirection = -1;
+        }
+        transform.localScale = new Vector3(facingDirection, 1, 1);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            jumpPressed = true;
+            jumpReleased = false;
+        }
+        else
+        {
+            jumpReleased = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
