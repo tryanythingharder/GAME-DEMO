3abd2db647e34a84a5df470cc163d87d740bee4a
xiaoxibukeai-LTC <1062147677@qq.com>
2025-11-13 22:53:55 +0800

    Initial commit or your message

---
+++ Assets/ParallaxManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class ParallaxLayer
+    {
+        public Transform layer;
+        [Range(0, 1)] public float parallaxFactor;
+    }
+
+    public ParallaxLayer[] layers;
+
+    public Transform camTransform;
+    private Vector3 lastCameraPosition;
+
+
+    void Start()
+    {
+        lastCameraPosition = camTransform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 cameraDelta = camTransform.position - lastCameraPosition;
+
+        foreach (ParallaxLayer layer in layers) 
+        {
+            float moveX = cameraDelta.x * layer.parallaxFactor;
+            float moveY = cameraDelta.y * layer.parallaxFactor;
+
+            layer.layer.position += new Vector3(moveX, moveY, 0);
+        }
+        lastCameraPosition = camTransform.position;
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,32 +5,37 @@ public class Player : MonoBehaviour
 {
     [Header("Components")]
     public Rigidbody2D rb;
-    public PlayerInput PlayerInput;
     public Animator anim;
 
     [Header("Movement Variables")]
-    public float speed;
-    public float jumpForce;
+    public float walkSpeed = 4f;
+    public float runSpeed = 8f;
+    public float jumpForce = 12f;
     public float jumpCutMultiplier = 0.5f;
-    public float normalGravity;
-    public float fallGravity;
-    public float jumpGravity;
+    public float normalGravity = 3f;
+    public float fallGravity = 5f;
+    public float jumpGravity = 2f;
 
-    public int facingDirection = 1;
-
-    public Vector2 moveInput;
+    [Header("Jump Settings")]
+    public int maxJumps = 2;
+    private int jumpCount;
     private bool jumpPressed;
     private bool jumpReleased;
 
     [Header("Ground Check")]
     public Transform groundCheck;
-    public float groundCheckRadius;
+    public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
     private bool isGrounded;
+
+    private Vector2 moveInput;
+    private bool runPressed;
+    private int facingDirection = 1;
 
     private void Start()
     {
         rb.gravityScale = normalGravity;
+        jumpCount = maxJumps;
     }
 
     void Update()
@@ -38,89 +43,119 @@ public class Player : MonoBehaviour
         Flip();
         HandleAnimations();
     }
-    
+
     void FixedUpdate()
     {
-        ApplyVariableGravity();
         CheckGrounded();
         HandleMovement();
         HandleJump();
+        ApplyVariableGravity();
     }
 
     private void HandleMovement()
     {
+        float speed = runPressed ? runSpeed : walkSpeed;
         float targetSpeed = moveInput.x * speed;
+
         rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
     }
 
     private void HandleJump()
     {
-        if (jumpPressed && isGrounded)
+        if (isGrounded && rb.linearVelocity.y <= 0.05f)
+        {
+            jumpCount = maxJumps;
+        }
+
+        if (jumpPressed && jumpCount > 0)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             jumpPressed = false;
             jumpReleased = false;
+
+            jumpCount--;
         }
-        if (jumpReleased)
+
+        if (jumpReleased && rb.linearVelocity.y > 0)
         {
-            if (rb.linearVelocity.y > 0)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
-            }
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
             jumpReleased = false;
         }
     }
 
     void ApplyVariableGravity()
     {
-        if (rb.linearVelocity.y < -0.1f)
-        {
+        if (rb.linearVelocity.y < -0.01f)
             rb.gravityScale = fallGravity;
-        }
-        else if (rb.linearVelocity.y > 0.1f)
-        {
+        else if (rb.linearVelocity.y > 0.01f)
             rb.gravityScale = jumpGravity;
-        }
         else
-        {
             rb.gravityScale = normalGravity;
-        }
     }
-
-
 
     void CheckGrounded()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
     }
 
     void HandleAnimations()
     {
-        anim.SetBool("isJumping", rb.linearVelocity.y > .1f);
-        anim.SetBool("isGrounded", isGrounded);
-        
-        anim.SetFloat("yVelocity",rb.linearVelocity.y);
+        bool isMoving = Mathf.Abs(moveInput.x) > 0.1f;
 
-        anim.SetBool("isIdle", Mathf.Abs(moveInput.x) < .1f && isGrounded);
-        anim.SetBool("isWalking", Mathf.Abs(moveInput.x) > .1f && isGrounded);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isJumping", rb.linearVelocity.y > 0.1f);
+
+        if (!isGrounded)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            return;
+        }
+
+        if (!isMoving)
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+        }
+        else if (isMoving && !runPressed)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+        }
+        else if (isMoving && runPressed)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", true);
+        }
     }
 
     void Flip()
     {
-        if(moveInput.x > 0.1f)
-        {
+        if (moveInput.x > 0.1f)
             facingDirection = 1;
-        }
-        else if(moveInput.x < -0.1f)
-        {
+        else if (moveInput.x < -0.1f)
             facingDirection = -1;
-        }
+
         transform.localScale = new Vector3(facingDirection, 1, 1);
     }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnRun(InputValue value)
+    {
+        runPressed = value.isPressed;
     }
 
     public void OnJump(InputValue value)
